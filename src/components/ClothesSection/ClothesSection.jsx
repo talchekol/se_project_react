29da196cf1ef90b667a4d841a543550a8271cd47
talchekol <tal000018@gmail.com>
2026-01-24 21:59:27 +0200

    p11 fb2

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -10,8 +10,8 @@ export default function ClothesSection({
     <div className="clothes-section">
       <div className="clothes-section__user">
         <p className="clothes-section__user-item">Your item</p>
-        <button onClick={onAddClick} className=" clothes-section__user-button">
-          +Add new
+        <button onClick={onAddClick} className="clothes-section__user-button">
+          + Add new
         </button>
       </div>
       <ul className="clothes-section__items">
